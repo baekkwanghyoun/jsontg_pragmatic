166176d60ef0ceb17ddccc7aa94db047bf6166c5
sean_rogue <silverjava@naver.com>
2021-12-01 22:25:27 +0900

    init

--- pragmatic/settings/deploy.py
+++ pragmatic/settings/deploy.py
@@ -15,15 +15,15 @@ env = environ.Env(
 )
 
 # reading .env file
-environ.Env.read_env(
-    env_file= os.path.join(BASE_DIR, '.env')
-)
+# environ.Env.read_env(
+#     env_file= os.path.join(BASE_DIR, '.env')
+# )
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
+SECRET_KEY = '1s$ji8q*j@f$fn3^+(3rp58%iudkujt^-p$xq38+)c#$x%d2ci'#read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
